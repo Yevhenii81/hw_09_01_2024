c847e56802673b50505a08887327507ee8e11659
Yevhenii81 <144814547+Yevhenii81@users.noreply.github.com>
2024-01-25 19:35:54 +0200

    Add files via upload

---
+++ hw_09_01_2024/hw_09_01_2024/hw_09_01_2024.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <ctime>
+#include <string.h>
+using namespace std;
+
+#include "Student.h"
+#include "Date.h"
+#include "Group.h"
+
+int main()
+{
+    Student studentA;
+    studentA.AddExam(7);
+    studentA.AddExam(12);
+    studentA.AddExam(9);
+
+    Student studentB;
+    studentB.AddExam(5);
+    studentB.AddExam(9);
+    studentB.AddExam(12);
+
+    Student studentС;
+    studentС.AddExam(2);
+    studentС.AddExam(9);
+    studentС.AddExam(7);
+
+    Group group;
+
+    // Добавление студентов в группу
+    group.AddStudent(studentA);
+    group.AddStudent(studentB);
+    group.AddStudent(studentС);
+
+    // Вывод информации о группе
+    cout << "Original Group:\n";
+    group.PrintGroup();
+
+    // Исключение студента из группы
+    cout << "\nExpelling a student:\n";
+    group.Expel();
+    group.PrintGroup();
+
+    Student copya;
+    copya = studentA;
+
+    Student copyb;
+    copyb = studentB;
+    Student copyс;
+    copyс = studentС;
+
+    Group copy;
+    copy = group;
+
+    cout << "\nCopied Group:\n";
+    copy.PrintGroup();
+}
